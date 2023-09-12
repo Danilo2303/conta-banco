3487f7e1c4aa2b964176547f9ba81128b8e58bd6
Danilo Alves <138080378+Danilo2303@users.noreply.github.com>
2023-09-11 22:57:23 -0300

    ContaBanco

--- src/ContaBanco.java
+++ src/ContaBanco.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaBanco {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor digite o número da sua Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor digite o número da sua conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor digite o nome do cliente: ");
+        String nomecliente = scanner.nextLine();
+
+        System.out.println("Por favor digite o Saldo da conta: ");
+        int saldo = scanner.nextInt(); 
+
+        scanner.close();
+
+    
+        System.out.println("Olá " + nomecliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia
+                + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
     }
 }
